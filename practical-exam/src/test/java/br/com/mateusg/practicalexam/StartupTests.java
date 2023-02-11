5d8aae6c84a923b3927a882428edebe138434de6
Mateus Gomes <magomescruz@gmail.com>
2023-02-11 11:49:40 -0300

    :tada: Spring template

---
+++ practical-exam/src/test/java/br/com/mateusg/practicalexam/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.mateusg.practicalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
