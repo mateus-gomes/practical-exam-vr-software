553660d3b08a9ac8b262676e38ba2db025ee168b
Mateus Gomes <magomescruz@gmail.com>
2023-02-12 10:14:23 -0300

    :sparkles: Add validation and convert rules to Client creation

--- practical-exam/src/main/java/br/com/mateusg/practicalexam/handler/CustomizedResponseEntityExceptionHandler.java
+++ practical-exam/src/main/java/br/com/mateusg/practicalexam/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.mateusg.practicalexam.handler;
 
 import br.com.mateusg.practicalexam.exception.ExceptionResponse;
+import br.com.mateusg.practicalexam.exception.NotADayOfTheMonthException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,12 +18,23 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleGenericExceptions(Exception exception, WebRequest request){
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
+        ExceptionResponse exceptionResponse = buildExceptionResponse(exception, request);
+
+        return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotADayOfTheMonthException.class)
+    public ResponseEntity<ExceptionResponse> handleNotADayOfTheMonthExceptions(Exception exception, WebRequest request){
+        ExceptionResponse exceptionResponse = buildExceptionResponse(exception, request);
+
+        return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    private ExceptionResponse buildExceptionResponse(Exception exception, WebRequest request){
+        return new ExceptionResponse(
                 LocalDateTime.now(),
                 exception.getMessage(),
                 request.getDescription(false)
         );
-
-        return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- practical-exam/src/main/java/br/com/mateusg/practicalexam/service/ClientService.java
+++ practical-exam/src/main/java/br/com/mateusg/practicalexam/service/ClientService.java
@@ -1,21 +1,39 @@
 package br.com.mateusg.practicalexam.service;
 
+import br.com.mateusg.practicalexam.exception.NotADayOfTheMonthException;
 import br.com.mateusg.practicalexam.model.Client;
 import br.com.mateusg.practicalexam.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 @Service
 public class ClientService {
-
     @Autowired
     private ClientRepository clientRepository;
 
 
     public void create(Client client) {
-        clientRepository.save(client);
+        clientRepository.save(executeClientRules(client));
+    }
+
+    private Client executeClientRules(Client client){
+        verifyIfNumberIsADayOfTheMonth(client.getInvoiceDueDate());
+        client.setClientLimit(convertToTwoDecimalPlaces(client.getClientLimit()));
+
+        return client;
+    }
+
+    private void verifyIfNumberIsADayOfTheMonth(int number){
+        if(number > 31){
+            throw new NotADayOfTheMonthException("Should be a valid day of the month!");
+        }
+    }
+
+    private Double convertToTwoDecimalPlaces(Double number){
+        return Math.round(number * 100.0) / 100.0;
     }
 
     public List<Client> findAll() {
