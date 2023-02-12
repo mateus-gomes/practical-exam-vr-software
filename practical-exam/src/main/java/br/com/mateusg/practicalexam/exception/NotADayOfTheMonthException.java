53d437caa3a252148db0190346cde0051e82f9a4
Mateus Gomes <magomescruz@gmail.com>
2023-02-12 11:26:48 -0300

    :recycle: Refactor: Change Response status for NotADayOfTheMonthException from 400 to 422

--- practical-exam/src/main/java/br/com/mateusg/practicalexam/exception/NotADayOfTheMonthException.java
+++ practical-exam/src/main/java/br/com/mateusg/practicalexam/exception/NotADayOfTheMonthException.java
@@ -3,7 +3,7 @@ package br.com.mateusg.practicalexam.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class NotADayOfTheMonthException extends RuntimeException {
       private String exception;
 
