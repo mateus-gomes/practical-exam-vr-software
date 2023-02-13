7e9865baa17c977e48c7a45b1cea4ff40954fb32
Mateus Gomes <magomescruz@gmail.com>
2023-02-13 09:55:52 -0300

    :recycle: Refactor: remove unused import from Client class

--- practical-exam/src/main/java/br/com/mateusg/practicalexam/model/Client.java
+++ practical-exam/src/main/java/br/com/mateusg/practicalexam/model/Client.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import org.springframework.data.annotation.PersistenceConstructor;
 
 @Entity
 @Table(name = "client")
