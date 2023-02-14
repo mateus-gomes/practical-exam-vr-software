c3a8cc0f1ebe04effcbbc4da592d2ae238575707
Mateus Gomes <magomescruz@gmail.com>
2023-02-13 22:24:33 -0300

    :recycle: Refactor: Remove unused imports

--- practical-exam/src/main/java/br/com/mateusg/practicalexam/controller/OrderClientProductController.java
+++ practical-exam/src/main/java/br/com/mateusg/practicalexam/controller/OrderClientProductController.java
@@ -1,20 +1,14 @@
 package br.com.mateusg.practicalexam.controller;
 
-import br.com.mateusg.practicalexam.exception.InvalidGivenIdException;
 import br.com.mateusg.practicalexam.handler.ErrorHandler;
 import br.com.mateusg.practicalexam.model.OrderClientProduct;
-import br.com.mateusg.practicalexam.service.ClientService;
 import br.com.mateusg.practicalexam.service.OrderClientProductService;
-import br.com.mateusg.practicalexam.service.OrderService;
-import br.com.mateusg.practicalexam.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/orders/items")
